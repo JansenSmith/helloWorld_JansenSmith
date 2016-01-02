3b23b0d5512ae323a4942b292cd8596f283878d6
madhephaestus <mad.hephaestus@gmail.com>
2016-01-02 16:37:53 -0500

    hjjkj/n/nlklkj

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,7 +1,4 @@
-
 //you print messages to the text output
 println "Hello World From BowlerStudio Terminal!"
-
 // or have it speak messages
-//BowlerKernel.speak("Welcome to Bowler Studio", 175.0, 120.0, 41.0, 1.0, 1.0); // you can modify the way it speaks with the other parameters
 BowlerKernel.speak("Welcome to Bowler Studio");
